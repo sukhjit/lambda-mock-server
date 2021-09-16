2d7ad8228e3e495c7ac862a01102cf7f912832fe
Sukhjit Singh <cassini2@gmail.com>
2021-09-16 19:15:40 +1000

    Update serverless

---
+++ cicd/bin/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -sL https://deb.nodesource.com/setup_14.x | bash
+
+if ! which node > /dev/null 2>&1; then
+    apt-get update && apt-get install -y nodejs
+fi
+
+echo "Container running..."
+tail -f /dev/null
