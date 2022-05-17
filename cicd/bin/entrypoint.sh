acec4eeecc260b9e6a1d8f00b24f1e406150778b
Sukhjit Singh <sukhjit.singh@sbs.com.au>
2022-05-17 16:10:40 +1000

    Use node 16

--- cicd/bin/entrypoint.sh
+++ cicd/bin/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_14.x | bash
+curl -sL https://deb.nodesource.com/setup_16.x | bash
 
 if ! which node > /dev/null 2>&1; then
     apt-get update && apt-get install -y nodejs
